30aa20a3170873e4ea9dab3e4cfee1e786c38651
Andrew Lovett-Barron <andrew@relaystudio.com>
2014-11-18 14:55:55 -0800

    Playing with Map syntax

--- map.go
+++ map.go
@@ -8,14 +8,17 @@ type Pin struct {
 
 var m map[string]Pin
 
+var t = map[string]Pin{
+	"TheOne" : { 123.2, 333.2 },
+	"TheTwo" : { 232.2, 444.2 }, // Why do I not need a comma here?
+}
+
 func main() {
 	m = make(map[string]Pin)
 	m["place"] = Pin{
-		1.01, 2.02,
+		1.01, 2.02, // Why do I need a comma here?
 	}
-	m["anotherPlace"] = Pin{
-		4.01, -122.02,
-	}
+	m["anotherPlace"] = Pin{ 4.01, -122.02 } // Comma as indicator of "Consider next line?"
 	fmt.Println(m["place"])
 	fmt.Println(m)
 }
